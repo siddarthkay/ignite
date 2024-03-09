16366ee033a52009e00142b39fe73611535cdbd8
Siddarth Kumar <siddarthkay@gmail.com>
2024-03-09 15:45:59 +0530

    init: storybook configuration setup

--- boilerplate/metro.config.js
+++ boilerplate/metro.config.js
@@ -1,14 +1,25 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+const { generate } = require('@storybook/react-native/scripts/generate');
+
+generate({
+  configPath: path.resolve(__dirname, './.storybook'),
+});
+
 
 config.transformer.getTransformOptions = async () => ({
   transform: {
     experimentalImportSupport: false,
     inlineRequires: true,
+    unstable_allowRequireContext: true,
   },
 });
+
+config.resolver.sourceExts.push('mjs');
 
 module.exports = config;
